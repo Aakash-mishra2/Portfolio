970de6ef3d876af39822b697dce43bd994fd3f5c
Aakash Mishra <aakashmishra.281@gmail.com>
2023-06-13 12:37:47 +0530

    Finished add icons and certificates link

--- src/Articles/Articles.js
+++ src/Articles/Articles.js
@@ -1,18 +1,22 @@
 import FrontList from "../front/components/FrontList";
+import vsn from "../icons/vision.png";
+import wbd from "../icons/web-design.png";
 
 export default function Articles() {
-    const ARTICLES = [{
-        id: 'a1',
-        name: 'Introduction to Full Stack Development',
-        icon: '',
-        linkTo: 'https://www.dropbox.com/s/bgps903rbazui58/Report%20on%20Web%20Developement%20%20-%20Introduction.pdf?dl=0'
-    },
-    {
-        id: 'a2',
-        name: 'My Vision for India: 2047',
-        icon: '',
-        linkTo: 'https://www.dropbox.com/s/mlnx69ge3wtcj2z/My%20Vision%20for%20India%202047.pdf?dl=0'
-    }];
+    const ARTICLES = [
+        {
+            id: 'a1',
+            name: 'My Vision for India: 2047',
+            icon: vsn,
+            linkTo: 'https://www.dropbox.com/s/mlnx69ge3wtcj2z/My%20Vision%20for%20India%202047.pdf?dl=0'
+        },
+        {
+            id: 'a2',
+            name: 'Internet and Web Development report',
+            icon: wbd,
+            linkTo: 'https://www.dropbox.com/s/bgps903rbazui58/Report%20on%20Web%20Developement%20%20-%20Introduction.pdf?dl=0'
+        }
+    ];
 
     return (<FrontList cards={ARTICLES} />);
 }
--- src/Training/Training.js
+++ src/Training/Training.js
@@ -1,31 +1,38 @@
 import FrontList from "../front/components/FrontList";
+import cpp from "../icons/c-.png";
+import algo from "../icons/neural.png";
+import dsa from "../icons/algorithm.png";
+import apd from "../icons/app-development.png";
 
 export default function Trainings() {
     const COURSES = [
         {
             id: 'programming_C++',
-            name: 'Programming in Modern C++, IIT Kharagpur, NPTEL',
+            name: 'Programming in Modern C++, Prof. Partha Pratim Das, IIT Kharagpur',
             description: 'Prof. Partha Pratim Das',
-            icon: ''
+            linkTo: 'https://www.dropbox.com/s/bhusc0ql2jb6g04/Programming%20in%20Modern%20C%2B%2B.jpg?dl=0',
+            icon: cpp
         },
         {
             id: 'comp_programming',
-            name: 'Introduction to Competitive Programming, IIT Gandhinagar, NPTEL',
+            name: 'Competitive Programming Introduction, Prof. Neeldhara Mishra, IIT Gandhinagar',
             description: 'Prof. Neeldhara Mishra',
-            icon: ''
+            linkTo: 'https://www.dropbox.com/s/k7gz3iao2zspuz5/Getting%20Started%20with%20Competitive%20Programming.jpg?dl=0',
+            icon: algo
         },
         {
             id: 'mern-stack',
             name: 'Full (MERN) Stack Web Development, Udemy',
             description: 'Dr. Angela Yu',
-            icon: '',
-            linkTo: 'https://www.dropbox.com/s/29dpbx39pq7ohaq/UdemyWebC.jpg?dl=0'
+            linkTo: 'https://www.dropbox.com/s/29dpbx39pq7ohaq/UdemyWebC.jpg?dl=0',
+            icon: apd
         },
         {
             id: 'dsa',
             name: 'Mastering Data Structures and Algorithms, Udemy',
             description: 'Dr. Abdul Bari',
-            icon: ''
+            icon: dsa,
+            linkTo: 'https://www.dropbox.com/s/jen2w0grhzzxzu9/UdemyAlgoC.jpg?dl=0'
         },
 
     ];
