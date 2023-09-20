28d0124622fc799e4226841e91d7ea5e17805dfc
Aakash Mishra <aakashmishra.281@gmail.com>
2023-09-20 15:00:32 +0530

    new modal features on project list page

--- src/shared/UIElements/Modal.js
+++ src/shared/UIElements/Modal.js
@@ -1,31 +1,30 @@
 import React from "react";
 import { CSSTransition } from "react-transition-group";
 import ReactDOM from "react-dom";
-
-import './styles/Modal.css';
 import Backdrop from "./Backdrop";
+import "./styles/Modal.css";
 
-
-function ModalOverlay(props) {
+const ModalOverlay = (props) => {
     const content = (
         <div className={`modal ${props.className}`} style={props.style}>
             <header className={`modal__header ${props.headerClass}`}>
                 {props.header}
             </header>
-            <form onSubmit={props.onSubmit ? props.onSubmit : event => event.preventDefault()}>
-                <div className={`modal__content ${props.contentClass}`}>
-                    <h2>{props.children}</h2>
-                </div>
-                <footer className={`modal__footer ${props.footerClass}`} >
-                    {props.footer}</footer>
-            </form>
-        </div>
-    );
-
+            <div className={`modal__content ${props.contentClass}`}>
+                <form onSubmit={props.onSubmit ? props.onSubmit : event => event.preventDefault()}>
+                    {props.children}
+                </form>
+            </div>
+            <footer className={`modal__footer ${props.footerClass}`}>
+                {props.footer}
+            </footer>
+        </div >
+    )
     return ReactDOM.createPortal(content, document.getElementById('modal'));
 }
 
-export default function Modal(props) {
+const Modal = (props) => {
+
     return (
         <React.Fragment>
             {props.show && <Backdrop onClick={props.closeBox} />}
@@ -40,4 +39,5 @@ export default function Modal(props) {
             </CSSTransition>
         </React.Fragment>
     );
-};
+}
+export default Modal;
