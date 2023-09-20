52b8ed31a0e138127ef1c331348ef37313486ef8
Aakash Mishra <aakashmishra.281@gmail.com>
2023-09-20 15:11:47 +0530

    new project details, techstack

--- src/projects/AllProjects.js
+++ src/projects/AllProjects.js
@@ -8,27 +8,28 @@ export default function AllProjects() {
         {
             id: 'thl234',
             title: 'Court Case Management System',
-            description: " No desc. ",
+            description: "Full MERN Stack web application to manage pending court cases for citizens and register new case applications online. ",
             image: ccms,
-            projectUrl: 'https://courtcase-manager.netlify.app/',
-            // repoLink: 'https://github.com/Aakash-mishra2/CCMS-frontEnd',
-            // READme: 'https://github.com/Aakash-mishra2/CCMS-frontEnd#readme'
+            projectUrl: 'https://yourccms.netlify.app/',
+            gitURL: 'https://github.com/Aakash-mishra2/CCMS-frontEnd',
+            techStack: "Frontend: ReactJS, Hooks , Axios, Redux, HTML-CSS  Backend: RestAPI, NodeJS, ExpressJS, Javascript, MongoDB ",
         },
         {
             id: 'thl234',
             title: 'Shopmate',
-            description: " No desc. ",
+            description: "Dynamic Shopping list web app to add and remove items. Save your shopping lists for future.",
             projectUrl: 'https://shopmate-sky.netlify.app/',
             image: shopm,
-            // repoLink: 'https://github.com/Aakash-mishra2/ShopMate-FrontEnd',
-            // READme: 'https://github.com/Aakash-mishra2/ShopMate-FrontEnd#readme'
+            gitURL: 'https://github.com/Aakash-mishra2/ShopMate-FrontEnd',
+            techStack: 'FrontEnd: ReactJS, ContextAPI, Javascript, HTML-CSS '
         },
         {
             id: 'jsc234',
             title: 'Co-curricular: Java swing calculator',
             image: jvc,
             description: " No desc. ",
-            // repoLink: 'https://github.com/Aakash-mishra2/Java_swing_Calculator',
+            gitURL: 'https://github.com/Aakash-mishra2/Java_swing_Calculator',
+            techStack: ''
         }
     ]
 
--- src/shared/FormComponents/Button.js
+++ src/shared/FormComponents/Button.js
@@ -9,7 +9,8 @@ const Button = props => {
             <a
                 className={`button button--${props.size || 'default'} ${props.inverse &&
                     'button--inverse'} ${props.danger && 'button--danger'}`}
-                href={props.href}
+                href={props.href} target={props.target}
+                onClick={props.onClick}
             >
                 {props.children}
             </a>
