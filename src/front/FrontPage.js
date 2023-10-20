b8856e29d28de40923c27f62469c06729afb9dc3
Aakash Mishra <aakashmishra.281@gmail.com>
2023-10-20 15:13:27 +0530

    Added intro section

--- src/front/FrontPage.js
+++ src/front/FrontPage.js
@@ -3,7 +3,7 @@ import prj from '../icons/project-management.png';
 import rsl from '../icons/results.png';
 import mlt from '../icons/multitasking.png';
 import trg from '../icons/online-learning.png';
-
+import React from "react";
 export default function FrontPage() {
     const CARDS = [
         {
@@ -32,5 +32,19 @@ export default function FrontPage() {
         }
     ]
 
-    return (<FrontList cards={CARDS} />)
+    return (
+    <React.Fragment>
+    <div className="max-w-3xl mx-auto">
+    <h1 className="text-3xl font-bold test-red-500 font-serif mt-10 ml-30">
+        <tt>Hey 👋, I'm Aakash Mishra</tt>
+    </h1>
+    <p className="text-base font-serif font-thin"><tt>
+        I am a CSE undergraduate and do Full Stack Web development using 
+        Javascript, React, Next, Redux, tailwindcss, HTML, Node.js and More. I have good understanding of 
+        Object oriented Design and analysis using C++ Data structure and Algorithms.
+    </tt></p>
+    </div>
+    <FrontList cards={CARDS} />
+    </React.Fragment>
+    )
 }
