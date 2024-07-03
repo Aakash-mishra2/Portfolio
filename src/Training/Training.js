59dc2b2d3044702510607d4eccf138fc5596c59a
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2024-07-03 15:29:37 +0530

    Update Training.js

--- src/Training/Training.js
+++ src/Training/Training.js
@@ -6,20 +6,20 @@ import apd from "../icons/app-development.png";
 
 export default function Trainings() {
     const COURSES = [
-        {
-            id: 'programming_C++',
-            name: 'Programming in Modern C++, Prof. Partha Pratim Das, IIT Kharagpur',
-            description: 'Prof. Partha Pratim Das',
-            linkTo: 'https://www.dropbox.com/s/bhusc0ql2jb6g04/Programming%20in%20Modern%20C%2B%2B.jpg?dl=0',
-            icon: cpp
-        },
-        {
-            id: 'comp_programming',
-            name: 'Getting Started in Competitive Programming , Prof. Neeldhara Mishra, IIT Gandhinagar',
-            description: 'Prof. Neeldhara Mishra',
-            linkTo: 'https://www.dropbox.com/s/k7gz3iao2zspuz5/Getting%20Started%20with%20Competitive%20Programming.jpg?dl=0',
-            icon: algo
-        },
+        // {
+        //     id: 'programming_C++',
+        //     name: 'Programming in Modern C++, Prof. Partha Pratim Das, IIT Kharagpur',
+        //     description: 'Prof. Partha Pratim Das',
+        //     linkTo: 'https://www.dropbox.com/s/bhusc0ql2jb6g04/Programming%20in%20Modern%20C%2B%2B.jpg?dl=0',
+        //     icon: cpp
+        // },
+        // {
+        //     id: 'comp_programming',
+        //     name: 'Getting Started in Competitive Programming , Prof. Neeldhara Mishra, IIT Gandhinagar',
+        //     description: 'Prof. Neeldhara Mishra',
+        //     linkTo: 'https://www.dropbox.com/s/k7gz3iao2zspuz5/Getting%20Started%20with%20Competitive%20Programming.jpg?dl=0',
+        //     icon: algo
+        // },
         {
             id: 'mern-stack',
             name: 'Full (MERN) Stack Web Development, Dr. Angela Yu, Udemy',
